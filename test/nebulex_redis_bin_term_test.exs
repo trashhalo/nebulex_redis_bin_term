4dd743a75465a95572e774953a68b03d22e949ca
Stephen Solka <stephen@standd.io>
2024-09-26 10:39:45 -0400

    Remove redundant test and clean up whitespace in nebulex_redis_bin_term_test.exs

--- test/nebulex_redis_bin_term_test.exs
+++ test/nebulex_redis_bin_term_test.exs
@@ -2,17 +2,13 @@ defmodule NebulexRedisBinTermTest do
   use ExUnit.Case
   doctest NebulexRedisBinTerm
 
-  test "greets the world" do
-    assert NebulexRedisBinTerm.hello() == :world
-  end
-
   test "get returns binary data" do
     object = %{a: "b"}
     object_bin = :erlang.term_to_binary(object)
-    
+
     NebulexRedisBinTerm.Cache.put("object_bin", object_bin)
     result = NebulexRedisBinTerm.Cache.get("object_bin")
-    
+
     assert is_binary(result)
     assert result == object_bin
     refute result == object
