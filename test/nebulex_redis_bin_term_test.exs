64fa7cbd257246c59bb3fa59afdef3f534b9caa8
Stephen Solka <stephen@standd.io>
2024-09-26 10:35:41 -0400

    Add test for binary data retrieval in NebulexRedisBinTermTest

--- test/nebulex_redis_bin_term_test.exs
+++ test/nebulex_redis_bin_term_test.exs
@@ -5,4 +5,16 @@ defmodule NebulexRedisBinTermTest do
   test "greets the world" do
     assert NebulexRedisBinTerm.hello() == :world
   end
+
+  test "get returns binary data" do
+    object = %{a: "b"}
+    object_bin = :erlang.term_to_binary(object)
+    
+    NebulexRedisBinTerm.Cache.put("object_bin", object_bin)
+    result = NebulexRedisBinTerm.Cache.get("object_bin")
+    
+    assert is_binary(result)
+    assert result == object_bin
+    refute result == object
+  end
 end
